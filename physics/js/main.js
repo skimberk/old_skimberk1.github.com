5934544e33de30c6c76ba6df455bdf53734fe55e
skimberk1 <skimberk1@gmail.com>
2012-11-05 07:08:26 -0500

    Added graph.

--- physics/js/main.js
+++ physics/js/main.js
@@ -5,4 +5,22 @@
 			mode: "overlay"
 		});
 	});
+
+	//Chart
+	var deathsArray = [
+		[2000, 41945],
+		[2001, 42196],
+		[2002, 43005],
+		[2003, 42643],
+		[2004, 42836],
+		[2005, 43443],
+		[2006, 42642],
+		[2007, 41059],
+		[2008, 37261],
+		[2009, 33808],
+		[2010, 32885]
+	];
+
+	var deathsContainer = $('.graph.deaths');
+	var deaths = $.plot(deathsContainer, [deathsArray]);
 })();
